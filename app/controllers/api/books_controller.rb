bc9e40d6ef63b124c4710bdb4f7905d8bddbafc1
Thomas Ng <D3icidal@gmail.com>
2018-10-29 19:29:29 -0400

    fixes books controller and normalizes author table

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,18 +1,42 @@
 class Api::BooksController < ApplicationController
   #index
   def index
-    @books = Books.all
+    @books = Book.all
     render json: @books
   end
 
   #show
   def show
-    @book = Books.find_by(id: params[:id])
+    @book = Book.find_by(id: params[:id])
     p @book
-    
+    render json: @book
+  end
 
 
   #Create
+  def create
+    @book = Book.new(
+      title: params[:title],
+      author: params[:author]
+      )
+    @book.save
+    render json: @book
+  end
+
   #update
+  def update
+    @book = Book.find_by(id: params[:id])
+    @book.update(
+      title: params[:title] || book.title,
+      author: params[:author] || book.author
+    )
+    render json: @book
+  end
+
   #Destroy
+  def destroy
+    @book = Book.find_by(id: params[:id])
+    @book.destroy
+    render json: "deleted"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,18 @@ Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
-    #CRUD
-    GET '/books' => 'books#index'
-    GET '/books/:id' => 'books#show'
-    POST '/books' => 'books#create'
-    PATCH '/books/:id' => 'books#update'
-    DELETE '/books/:id' => 'books#destroy'
+    #CRUD Books
+    get '/books' => 'books#index'
+    get '/books/:id' => 'books#show'
+    post '/books' => 'books#create'
+    patch '/books/:id' => 'books#update'
+    delete '/books/:id' => 'books#destroy'
+
+    #CRUD Authors
+    get '/authors' => 'authors#index'
+    get '/authors/:id' => 'authors#show'
+    post '/authors' => 'authors#create'
+    patch '/authors/:id' => 'authors#update'
+    delete '/authors/:id' => 'authors#destroy'
   end
 end
